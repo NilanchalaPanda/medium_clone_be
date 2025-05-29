334d3606f3ebbfa08fe1abfc1a40852a88bcc3ec
NilanchalaPanda <nilanchalpanda2003@gmail.com>
2025-05-29 20:29:46 +0530

    fix: add users entity to article module imports to complete user association

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -3,9 +3,10 @@ import { ArticleController } from '@app/article/article.controller';
 import { ArticleService } from '@app/article/article.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Article } from '@app/article/article.entity';
+import { Users } from '@app/user/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Article])],
+  imports: [TypeOrmModule.forFeature([Article, Users])],
   controllers: [ArticleController],
   providers: [ArticleService],
 })
